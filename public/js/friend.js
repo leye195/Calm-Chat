90651d5bba738b8c9bf06b56f39e4485a0274848
leye195 <leye195@naver.com>
2019-12-24 19:14:23 +0900

    191224(fixed some code and added friend_list)

--- public/js/friend.js
+++ public/js/friend.js
@@ -1,15 +1,24 @@
 const arrow=document.querySelector(".friend_arrow"),add_friend=document.querySelector(".js_friend"),
-f_div=document.querySelector(".f_div"),f_input=document.querySelector(".friend_id"),clear_btn=document.querySelector(".friend_id+span");
+f_div=document.querySelector(".f_div"),f_input=document.querySelector(".friend_id"),
+clear_btn=document.querySelector(".friend_id+span"),friend_info=document.querySelector(".friend_info")
+p_s=document.querySelectorAll(".info_form p"),add_btn=document.querySelector(".info_form button"),
+f_ul=document.querySelector(".friend_ul"),friend_div=document.querySelector(".friend_div");
 function checkArrow(e){
     const {target}=e;
     if(target.classList.contains("up")){
         target.classList.remove("up");
         target.classList.add("down");
         target.src="/img/down_arrow.png";
+        f_ul.classList.add("f_up");
+        f_ul.classList.remove("f_down");
+        friend_div.style.borderBottom="1px solid #ececec";
     }else if(target.classList.contains("down")){
         target.classList.remove("down");
         target.classList.add("up");
         target.src="/img/up_arrow.png";
+        f_ul.classList.add("f_down");
+        f_ul.classList.remove("f_up");
+        friend_div.style.borderBottom="none"
     }
 }
 function open_addFriend(){
@@ -28,24 +37,67 @@ function create_friend_tag(user_id){
     li.appendChild(user_div);
     return li;
 }
-function handleAdd(e){
+function checkSame(n1,n2){
+    console.log(n1,n2);
+    return n1===n2;
+}
+function handleInfo(e){
     const {target}=e;
-    const f_list=document.querySelector(".friend_ul");
     if(e.keyCode===13){
-        fetch(`user/${target.value}`)
+        fetch(`friend/${target.value}`)
         .then((data)=>data.text())
         .then((data)=>{
             const result=JSON.parse(data);
-            console.log(result);
+            //console.log(result);
             if(result['error']===0&&result['success']===1){
-                alert(JSON.stringify(result['result']));
-                f_list.appendChild(create_friend_tag(result['result']['email']));
+                //alert(JSON.stringify(result['result']));
+                friend_info.classList.toggle("hide");
+                p_s[1].textContent=result['result']['email'];
+                if(checkSame(document.querySelector(".profile_main .username").textContent,p_s[1].textContent)){
+                    add_btn.disabled=true;
+                }
             }
         })
         target.value="";
     }
 }
+function handleAdd(e){
+    const{target}=e,f_list=document.querySelector(".friend_ul");
+    const my=document.querySelector(".profile_main .username").textContent;
+    if(!checkSame(my,p_s[1].textContent)){
+        const options = {
+            method: 'post',
+            body: JSON.stringify({my:my,email:p_s[1].textContent}),
+            headers: {'Content-type': 'application/json'},
+        }
+        fetch(`/add_friend`,options)
+        .then((res)=>res.json())
+        .then((data)=>{
+            if(data['error']===0){
+                alert("Add Success");
+                friend_info.classList.toggle("hide");
+                f_list.appendChild(create_friend_tag(p_s[1].textContent));
+            }else if(data['error']===3){
+                alert(data['msg']);    
+            }
+        })
+    } 
+}
+function load_friendList(){
+    const myid=document.querySelector(".profile_main .username").textContent;
+    if(myid!=="Username"){
+        fetch(`/friend_list/${myid}`)
+        .then((res)=>res.text())
+        .then((res)=>{
+            const result=new Set((JSON.parse(res))['result']);
+            result.forEach(item => {
+                f_ul.appendChild(create_friend_tag(item));
+            }); 
+        })
+    }
+}
 function init(){
+    load_friendList();
     arrow.addEventListener("click",checkArrow);
     add_friend.addEventListener("click",open_addFriend);
     f_input.addEventListener("keyup",()=>{
@@ -55,7 +107,12 @@ function init(){
         clear_btn.style.visibility = "hidden";
         f_input.value="";
     });
-    f_input.addEventListener("keydown",handleAdd);
+    f_input.addEventListener("keydown",handleInfo);
+    p_s[0].addEventListener("click",()=>{
+        friend_info.classList.toggle("hide");
+        add_btn.disabled=false;
+    });
+    add_btn.addEventListener("click",handleAdd);
 }
 init();
 
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,16 @@
 module.exports = function(app,User){
-//get user information,login Get Request
+    //get: user information,login Get Request
     app.get("/login",(req,res)=>{
         const {email,pwd}=req.query;
-        //console.log(req.query);
         User.findOne({email:email,password:pwd},(err,data)=>{
             if(err)res.status(500).json({error:1,msg:'DB Error'});
             if(data===null)res.json({error:2,msg:'User can not find'});
-            if(data!==null)res.json({error:0,success:1});
+            if(data!==null)res.json({error:0,success:1,result:data});
         });
     });
-    //User SignUp Post Request 
+    //post: User SignUp Post Request 
     app.post("/signup",(req,res)=>{
         const email=req.body.email,pwd=req.body.pwd;
-        //console.log(req.body);
         User.find({email:email,password:pwd},(err,data)=>{
             if(err)res.status(500).json({error:1,msg:"DB Error"});
             if(data.length!==0){res.json({error:2,msg:"User Already Exists"});}
@@ -30,24 +28,44 @@ module.exports = function(app,User){
             }
         });
     });
-    //find User id to add friend
-    app.get('/user/:user_id',(req,res)=>{
+    //get: user's friend list
+    app.get('/friend_list/:user_id',(req,res)=>{
+        const{user_id}=req.params;
+        console.log(user_id);
+        User.findOne({email:user_id},{_id:0,email:0,password:0},(err,data)=>{
+            if(err)res.status(500).json({error:1,msg:"DB Error"});
+            if(data===null)res.json({error:2,msg:'User can not find'});
+            res.json({error:0,success:1,result:data['friends']});
+        })
+    });
+    //get: find User id to add friend
+    app.get('/friend/:user_id',(req,res)=>{
         const {user_id}=req.params;
-        console.log(req.params);
         User.findOne({email:user_id},(err,data)=>{
             if(err)res.status(500).json({error:1,msg:"DB Error"});
-            if(data.length===0){res.json({error:2,msg:"User does not exists"});}
-            else{
-                res.json({error:0,success:1,result:data});
-            }
+            if(data===null){res.json({error:2,msg:"User does not exists"});}
+            else{res.json({error:0,success:1,result:data});}
         })
     })
+    //post: add to friend list
     app.post('/add_friend',(req,res)=>{
         const {my,email}=req.body;
-        User.update({email:my},{
-            
-        },(err,output)=>{
+        User.find({email:my},(err,data)=>{
             if(err)res.status(500).json({error:1,msg:"DB Error"});
+            //console.log(data[0]['friends']);
+            const friends=data[0]['friends'];
+            if(friends.indexOf(email)!==-1){
+                res.json({error:3,msg:"Already your friend"});
+            }else{
+                User.update({email:my},{
+                    $push:{friends:email}
+                },(err,output)=>{
+                    if(err)res.status(500).json({error:1,msg:"DB Error"});
+                    if(!output.n)res.status(404).json({ error:2,msg:"user not found"});
+                    res.json({error:0,msg:"friend list updated"});
+                });
+            }
         });
+        
     })
 }
